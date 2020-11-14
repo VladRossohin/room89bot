79a2119f60af11fabdc35c2eb23c988df3d3d431
Vlad Rossohin <rossohin@auris.rfe.bsu.by>
2020-11-14 14:05:47 +0300

    writin users in db

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 require("dotenv").config();
 
+const User = require("./users");
+const mongoose = require("mongoose");
+const chat_id = "@room89_bot";
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
 var TelegramBot = require("node-telegram-bot-api"),
- telegram = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+  telegram = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-telegram.onText("/kod_stolovka", (msg) => {
- telegram.sendMessage(msg.chat.id, "Kod Stolovka");
+telegram.onText(/\/start/, (msg) => {
+  const user = new User({
+    id: msg.from.id,
+    first_name: msg.from.first_name,
+    last_name: msg.from.last_name,
+    username: msg.from.username,
+    language_code: msg.from.language_code,
+  });
+
+  console.log(user.username);
+  console.log(`is in db: ${User.findOne({ username: user.username }) != null}`);
+
+  if (
+    User.findOne({
+      id: user.id,
+    }) == null
+  ) {
+    user.save();
+  }
+  telegram.sendMessage(
+    msg.chat.id,
+    `АХАХАХАХАХХАХАХАХАХАХХАХАХАХАХ \nИщи себя в прошмандовках Азербайджана, \n${user.first_name.toUpperCase()} ${user.last_name.toUpperCase()}`
+  );
 });
 
-telegram.on("text", (message) => {
- telegram.sendMessage(message.chat.id, "Hello world");
+telegram.onText(/\/kod_stolovka/, (msg) => {
+  // telegram.sendMessage(msg.chat.id, "Kod Stolovka");
+  for (var user of User.find()) {
+    telegram.sendMessage(user.id, "KOD STOLOVLKA");
+  }
 });
+
+telegram.on("polling_error", (err) => {
+  console.log(err);
+  console.log("------");
+});
+
+// telegram.on("text", (message) => {
+//   telegram.sendMessage(message.chat.id, "Hello world");
+// });
---
+++ users.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema();
+UserSchema.add({
+  id: {
+    type: Number,
+    required: true,
+  },
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  language_code: {
+    type: String,
+  },
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
